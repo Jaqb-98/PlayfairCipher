e6c2d64f87db45a2d57696bb0b913bc271e9ee7b
Jakub Abramczyk <kubowianin@gmail.com>
2020-12-11 12:47:31 +0100

    Add project files.

---
+++ PlayfairCipher/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PlayfairCipher
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Key: ");
+            var key = Console.ReadLine().ToLower();
+            var playfair = new PlayFair();
+            Console.Write("text: ");
+            string txt = Console.ReadLine().ToLower();
+
+            playfair.SetKey(key);
+            playfair.KeyGen();
+            string e = "";
+            if (txt.Length % 2 == 0)
+            {
+                e = playfair.EncryptMessage(txt);
+
+                Console.WriteLine("Encryption: " + e);
+            }
+            else
+            {
+                txt = txt + 'x';
+                e = playfair.EncryptMessage(txt);
+            }   
+
+            Console.WriteLine("Decryption: " + playfair.DecryptMessage(e));
+
+            Console.ReadLine();
+        }
+    }
+}
